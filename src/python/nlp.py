60c1afb197cde5f43c8ee0dfcb6189a7219a23df
Tobias  Jönsson <tobbe-j93@hotmail.com>
2021-05-26 14:28:49 +0200

    function created with search and result working

--- src/python/nlp.py
+++ src/python/nlp.py
@@ -6,96 +6,75 @@ print()
 nlp = spacy.load("en_core_web_lg")
 matcher = PhraseMatcher(nlp.vocab, attr= 'LOWER')
 
-### list to store the search words from user and be able to match the texts from the DB.
-terms = []
-
 ### raw search input getting parsed to spacy.Doc object
-search_input = "she is a human and she apple played chess against rita and when or what if she likes playing chess. Rita loves movies."
+doc = """The "Greek Gift" is a common chess theme, but the player making the sacrifice usually has more than one piece developed. Pro tip: If ...Ng4+ just hangs the knight and ...Qh4+ isn't possible, don't waste your time even thinking about playing ...Bxh2+. An attacking master like Marshall had no chance either.
 
+On top of that, everyone recognizes the importance of piece activity and development, yet Marshall did not touch a piece on his queenside until move 17. Four moves later, he resigned.
+
+Some miniatures feature brilliant attacks, and some... do not. Marshall lost the next game as well, and his championship efforts were over.
+
+Game 1, Emanuel Lasker vs. David Janowsky, 1910
+In his match vs. David Janowsky, Lasker somehow won even more easily than against Marshall, again 8-0 but this time with just three draws. And Janowsky's troubles started right in game one.
+
+An aggressive player, Janowsky played Siegbert Tarrasch's defense to the Queen's Gambit (3...c5), where Black accepts an isolated d-pawn in exchange for piece activity. After 16 moves, however, Janowsky ended up with two isolated pawns on the queenside. Facing a difficult defensive task in the heavy piece endgame, Janowsky tried to shore up the position with the blunderous 19...Rd6??
+
+This move simply loses a piece, in a tactic that appeared on the board prior to Janowsky throwing in the towel."""
+
+search_input = """information about chess and the player named Janowsky"""
+
+def sim_res_search(search_input, doc):
+### list to store the search words from user and be able to match the texts from the DB.
+    terms = []
 ### Lemmatization of the search input , added to tweek the matching slightly further.
 
-lem_doc = nlp(search_input)
-for token in lem_doc:
-    lem_string = token.lemma_
-    search_input = search_input + ' ' + lem_string
+    lem_doc = nlp(search_input)
+    for token in lem_doc:
+        lem_string = token.lemma_
+        search_input = search_input + ' ' + lem_string
 
-print(search_input + '\n')
-search_string = nlp(search_input)
-
-
+    #print(search_input + '\n')
+    search_string = nlp(search_input)
+        
 ### cleans the search from stop words and punctuation and adds to terms list for matching processing of texts.
+    search_string_cleaned = [token for token in search_string if not token.is_stop and not token.is_punct]    
 
-search_string_cleaned = [token for token in search_string if not token.is_stop and not token.is_punct]
-
-for token in search_string_cleaned:
-    terms.append(token.text)
-
+    for token in search_string_cleaned:
+        terms.append(token.text)
     
-patterns = [nlp.make_doc(text) for text in terms]
-matcher.add("TerminologyList", patterns)
+    patterns = [nlp.make_doc(text) for text in terms]
+    matcher.add("TerminologyList", patterns)
+### take in texts as a list..    
+    text_list = [] 
+    text_list.append(doc)
+### list containing the counter values of the words matched only for debugging
+    d = []
 
-### take in texts as a list..
-doc1 = """I liked the movies etc The movie had good direction APPLE The movie was amazing i.e.
-            The movie was average direction was not bad The cinematography was nice. i.e.
-            The movie was a bit lengthy  otherwise fantastic  etc etc """
-
-doc2 = """Chess is a great game, they play the game with two players. The game against Rita is a famous one and when if they were playing and likes playing chess."""
-
-
-doc3 = """But they're still human, which has meant some poorly played games throughout the years, by both champions and challengers, players who won the match or lost it. Here are five of the worst World Chess Championship games ever played """
-
-doc4 = """Hey i will not have any match"""
-
-doc5 = """The beginning of Smyslov’s chess career was quite unique. He learned chess at the age of six from his father, but for the next eight years, he did not play a single game outside his home. As a result, Smyslov spent his formative years playing with his father, a strong club player, and studying chess books. Smyslov later wrote that his father’s library had more than 100 chess books, and he studied them all, from the “old masters” (Paul Morphy and Adolf Anderssen) to the top players of the time (Jose Capablanca, Alexander Alekhine, and Aron Nimzowitsch). By the time Smyslov finally started to play in regular tournaments, he already had a solid knowledge of chess classics, and it paid off in his games.
-
-For example, the very first of Smyslov’s games to appear in a chess publication has a strong similarity to a famous victory by Akiba Rubinstein:"""
-
-
-print("We are Matches: \n")
-text_list = [] 
-text_list.append(doc1)
-text_list.append(doc2)
-text_list.append(doc3)
-text_list.append(doc4)
-text_list.append(doc5)
-### list containing the counter values of the words matched
-d = []
-
-text_matches = []
-### looping trough the texts to look for matches using the phrasematcher instanced with the variable matcher.
-for texts in text_list:
-    doc = nlp(texts)
-    matches = matcher(doc)
-    for match_id, start, end in matches:
-        span = doc[start:end]
-        string_id = nlp.vocab.strings[match_id]
-        #print(span.text)
-        #print(span.sent)
-    
-        d.append((string_id, span.text))
-      
-    
-    if matches:
-        text_matches.append(doc)
+    #text_matches = []
+    ### looping trough the texts to look for matches using the phrasematcher instanced with the variable matcher.
+    for texts in text_list:
+        doc = nlp(texts)
+        matches = matcher(doc)
         
-        
-        print("spacy sim: ", search_string.similarity(doc))
-        
-            
-            
-        print("Match ID: {}\nString ID: {}\nStart: {}\nEnd: {}\nText: {}\nSentence: {}\nFull Text: {} ".format(
-        match_id, string_id, start, end, span.text, span.sent, doc))
-        print("\n".join(f'{i[0]} {i[1]} ({j})' for i,j in Counter(d).items()))
+        ### for loop for prints only
+        for match_id, start, end in matches:
+            span = doc[start:end]
+            string_id = nlp.vocab.strings[match_id]       
+            d.append((string_id, span.text))        
+        if matches:
+            sim_result = search_string.similarity(doc)
+            #text_matches.append((sim_result, doc))
+            #print("SimResult: {} \nMatch ID: {}\nString ID: {}\nStart: {}\nEnd: {}\nText: {}\nSentence: {}\nFull Text: {} ".format(
+            #sim_result, match_id, string_id, start, end, span.text, span.sent, doc))
+            print("\n".join(f'{i[0]} {i[1]} ({j})' for i,j in Counter(d).items()))             
+        else:
+            sim_result = 0.0
+    #for every_match in text_matches:
+    #    print(every_match)                       
+    return sim_result
 
+res = sim_res_search(search_input, doc)
 
-
-
-for every_match in text_matches:
-    
-    print(every_match)
-
-
-
+print(res)
 
 
 
