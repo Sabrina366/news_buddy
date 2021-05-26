6365bd292a0035371a3039548cf7f684b50268d6
Irena <i.ilvesten@hotmail.com>
2021-05-26 10:49:51 +0200

    Added Nav-bar and logo

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
+    <MainMenu/>
     <router-view/>
   </div>
 </template>
 
-<script setup>
-
+<script>
+import MainMenu from "./components/MainMenu.vue";
+export default {
+  name: 'App',
+  components: {
+    MainMenu
+  }
+}
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
---
+++ src/frontend/src/components/MainMenu.vue
@@ -0,0 +1,59 @@
+<template>
+
+ <nav class="navbar">
+    <div class="logo">
+    <a href="/"><img class="icon" src="src/assets/NewsBuddy-logo.png" alt="Icon"></a>
+   </div>
+    <a href="/">Create Content</a>
+    <a href="/">Articles</a>
+  </nav>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.navbar{
+  font-size: 20px;
+  margin: 0px;
+  justify-content: space-around;
+  align-items: center;
+  display: flex;
+}
+.logo{
+  float: left;
+}
+.icon {
+  padding: 15px;
+  background-size: cover;
+  width: 30%;
+  margin-top: 20px;
+}
+
+  
+.navbar a{
+  color: gray;
+  text-decoration: none;
+  padding: 0px 5px;
+}
+
+.navbar a:hover{
+  color: rgb(139, 164, 233);
+}
+
+
+@media (max-width: 1000px) {
+  .navbar {
+      font-size: 15px;
+      flex-direction: row;
+      align-items: center;
+      flex-wrap: wrap;
+      display: flex;
+      justify-content: space-between;
+  }
+}
+
+</style>
