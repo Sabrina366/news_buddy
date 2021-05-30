9754f3bb37b7e07f26d046c60935a133b28ec1fa
Ellinor <ellinorannmo@gmail.com>
2021-05-30 21:42:39 +0200

    grundläggande fix - ej drag/drop lr design

---
+++ src/frontend/src/components/addArticle.vue
@@ -0,0 +1,59 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <input type="author" placeholder="Author" required v-model="author">
+        <input type="date" placeholder="Published date" required v-model="date">
+        <input type="url" placeholder="Link to article" required v-model="url">
+        <textarea type="article" placeholder="Article" required v-model="article"></textarea>
+        
+
+        <div class="submit">
+            <button>Submit article</button>
+        </div>
+    </form>
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            author: '' ,
+            date: '',
+            article: '',
+        }
+    },
+    
+
+    methods: {
+    handlesubmit(){
+        
+
+
+    }
+    }
+}
+        
+
+
+
+
+</script>
+
+<style scoped>
+
+button{
+    background: rgb(87, 87, 192);
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+
+}
+
+.submit{
+    text-align: center;
+}
+
+
+</style>
