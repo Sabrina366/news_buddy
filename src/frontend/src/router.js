fdea85b801f156e0c6a5623916514b8a640cb4c1
Ellinor <ellinorannmo@gmail.com>
2021-05-30 19:09:38 +0200

    fix

--- src/frontend/src/router.js
+++ src/frontend/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '/src/components/Home.vue'
 import Article from '/src/components/Article.vue'
+import addArticle from '/src/components/addArticle.vue'
 const routes = [
     {
         path: '/',
@@ -11,7 +12,13 @@ const routes = [
         path: '/articles/:id/:title',
         name: 'Article',
         component: Article,
-    }
+    },
+    {
+        path: '/addArticle',
+        name: "addArticle",
+        component: addArticle,
+    },
+    
 ]
 const router = createRouter({
     history: createWebHistory(),
