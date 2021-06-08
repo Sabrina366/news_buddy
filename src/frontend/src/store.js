51c4cfc9f8f68733010d682c81d65daf10e03596
Ellinor <ellinorannmo@gmail.com>
2021-06-08 18:10:59 +0200

    mm

--- src/frontend/src/store.js
+++ src/frontend/src/store.js
@@ -23,6 +23,10 @@ const store = createStore({
         removeArticle(state, articleToRemove){
             state.articles = state.articles.filter(article => article != articleToRemove)
           },
+        setArticle(state, article){
+            state.article = article
+
+        }
        
     },
     actions:{
@@ -51,8 +55,18 @@ const store = createStore({
             let resData = 'resource deleted...';
       
             return resData;
-        }
-    }
+        },
+        async addArticle({state}) {
+            let res = await fetch(state.urls.springUrl + '/spring/api/articles', {
+                method: 'post',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify(state.article)
+            })
+            let data = await res.json()
+            state.article = data
+        },
+    },
+    
 })
 
 export default store
